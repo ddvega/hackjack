65bf31bc81de3e10c9d9d83fd839836375e8571a
David Vega <daviddvega@gmail.com>
2021-07-17 22:32:16 -0500

    gameplay screen added. Card dealing working.

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -8,6 +8,7 @@ import { Navbar } from './components/Navbar';
 import { theme } from './styles/theme';
 import { Account } from './screens/Account';
 import { Settings } from './screens/Settings';
+import { GamePlay } from './screens/GamePlay';
 
 export const Router = () => {
   return (
@@ -21,6 +22,7 @@ export const Router = () => {
             <Route path="/users" component={Users} />
             <Route path="/account" component={Account} />
             <Route path="/settings" component={Settings} />
+            <Route path="/gameplay" component={GamePlay} />
           </Switch>
         </ThemeProvider>
       </>
