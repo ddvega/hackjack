ebeafa83b1387ac07e626eb44a7b45258afb1f7b
David Vega <daviddvega@gmail.com>
2021-07-18 13:14:22 -0500

    gameplay screen added. Card dealing working.

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -2,29 +2,33 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
+import { UserProvider } from './store/UserProvider';
+
 import { Home } from './screens/Home';
 import { Users } from './screens/Users';
 import { Navbar } from './components/Navbar';
 import { theme } from './styles/theme';
 import { Account } from './screens/Account';
-import { Settings } from './screens/Settings';
+import { Settings } from './components/Settings';
 import { GamePlay } from './screens/GamePlay';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/users" component={Users} />
-            <Route path="/account" component={Account} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/gameplay" component={GamePlay} />
-          </Switch>
-        </ThemeProvider>
+        <UserProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/users" component={Users} />
+              <Route path="/account" component={Account} />
+              <Route path="/settings" component={Settings} />
+              <Route path="/gameplay" component={GamePlay} />
+            </Switch>
+          </ThemeProvider>
+        </UserProvider>
       </>
     </BrowserRouter>
   );
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,20 +1,59 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+// import { useAuth0 } from '@auth0/auth0-react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Card, CardMedia, Box } from '@material-ui/core';
+import { useUser } from '../store/UserProvider';
 
+const useStyles = () => {
+  const styles = makeStyles((theme) => ({
+    card: {
+      maxWidth: 500,
+      maxHeight: 200,
+    },
+    media: {
+      paddingTop: '1%', // 16:9
+    },
+  }));
+
+  return styles();
+};
 export const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  // const { user, isAuthenticated, isLoading } = useAuth0();
+  const styles = useStyles();
+  const { picture, username, email, isAuthenticated } = useUser();
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
+  // if (isLoading) {
+  //   return <div>Loading ...</div>;
+  // }
 
   return (
-    isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    )
+    <>
+      {isAuthenticated && (
+        <Container maxWidth="xs">
+          <Card className={styles.card}>
+            <CardMedia className={styles.media}>
+              <Box
+                display="flex"
+                flexDirection="row"
+                alignContent="center"
+                alignItems="center"
+                justify="center"
+                m={-1}
+                p={2}
+              >
+                <Box>
+                  <img src={picture} alt="" height="100px" width="100px" />
+                </Box>
+
+                <Box ml={2}>
+                  <h3>{username}</h3>
+                  <h3>{email}</h3>
+                </Box>
+              </Box>
+            </CardMedia>
+          </Card>
+        </Container>
+      )}
+    </>
   );
 };
